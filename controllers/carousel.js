3d2a823f6f3c058fe6647a4d56c2b059b622d6bc
viveloper <ucsik1234@gmail.com>
2020-07-18 10:30:14 +0900

    changed resource url

--- controllers/carousel.js
+++ controllers/carousel.js
@@ -1,32 +1,32 @@
 const carouselItems = [
   {
-    img: 'http://localhost:5000/CarouselMedia/OneDay_1920774.jpg',
-    video: 'http://localhost:5000/CarouselMedia/OneDay_1280720.mp4',
+    img: '/CarouselMedia/OneDay_1920774.jpg',
+    video: '/CarouselMedia/OneDay_1280720.mp4',
     use: 'home',
   },
   {
-    img: 'http://localhost:5000/CarouselMedia/SteelRain_1920774.jpg',
-    video: 'http://localhost:5000/CarouselMedia/SteelRain_1280720.mp4',
+    img: '/CarouselMedia/SteelRain_1920774.jpg',
+    video: '/CarouselMedia/SteelRain_1280720.mp4',
     use: 'home',
   },
   {
-    img: 'http://localhost:5000/CarouselMedia/Peninsula_1920774.jpg',
-    video: 'http://localhost:5000/CarouselMedia/Peninsula_1280720.mp4',
+    img: '/CarouselMedia/Peninsula_1920774.jpg',
+    video: '/CarouselMedia/Peninsula_1280720.mp4',
     use: 'home',
   },
   {
-    img: 'http://localhost:5000/CarouselMedia/OneDay_1920420.jpg',
-    video: 'http://localhost:5000/CarouselMedia/OneDay_1280720.mp4',
+    img: '/CarouselMedia/OneDay_1920420.jpg',
+    video: '/CarouselMedia/OneDay_1280720.mp4',
     use: 'movie',
   },
   {
-    img: 'http://localhost:5000/CarouselMedia/SteelRain_1920420.jpg',
-    video: 'http://localhost:5000/CarouselMedia/SteelRain_1280720.mp4',
+    img: '/CarouselMedia/SteelRain_1920420.jpg',
+    video: '/CarouselMedia/SteelRain_1280720.mp4',
     use: 'movie',
   },
   {
-    img: 'http://localhost:5000/CarouselMedia/Peninsula_1920420.jpg',
-    video: 'http://localhost:5000/CarouselMedia/Peninsula_1280720.mp4',
+    img: '/CarouselMedia/Peninsula_1920420.jpg',
+    video: '/CarouselMedia/Peninsula_1280720.mp4',
     use: 'movie',
   },
 ];
